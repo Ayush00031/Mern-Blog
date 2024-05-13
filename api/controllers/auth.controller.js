fbe3adb7fe02fdf8a6179e96883990977be38274
Ayush00031 <99634545+Ayush00031@users.noreply.github.com>
2024-05-13 19:06:37 +0530

    complete create a post page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,6 +52,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: validUser._id,
+        isAdmin: validUser.isAdmin,
       },
       process.env.JWT_SECRET
     );
@@ -98,7 +99,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
